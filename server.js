2771bf3ced6b8537a526a9bc61ead7dcdfbc4b2c
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-05-14 20:56:22 -0400

    commitetd for static page

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const {UserDetail} = require('./models');
 //const {ActTypeDetail} = require('./models');
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 
 // GET requests to /restaurants => return 10 restaurants
