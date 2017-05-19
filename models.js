0bb27c741ead5a8a70fa46976f8c8a33b2ebb1b3
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-05-19 19:23:02 -0400

    travis

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// this is our schema to represent a restaurant
+// this is our schema to represent a user
 const  userSchema = mongoose.Schema({
   username: {type: String},
   name: {
@@ -26,23 +26,16 @@ const  userSchema = mongoose.Schema({
    acttype:{type: String}
 });
 
-
-//userSchema.index({ accountCode: 1, ssn: 1,acttype:1}, { unique: true });
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
 // allow us to define properties on our object that manipulate
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
+// this virtual grabs the most recent grade for a user.
 userSchema.virtual('addressString').get(function() {
   return `${this.address.street} ${this.address.city}`.trim()});
 userSchema.virtual('userNameString').get(function() {
   return `${this.name.firstName} ${this.name.lastName}`.trim()});
-
-// this virtual grabs the most recent grade for a restaurant.
-// blogPostSchema.virtual('grade').get(function() {
-//   const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
-//   return gradeObj.grade;
-// });
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
@@ -66,41 +59,8 @@ userSchema.methods.apiRepr = function() {
   };
 }
 
-
-// userSchema.methods.apiAcctType = function() {
-
-//   return {
-  
-//     acttype:this.acttype
-//   };
-// }
-
-
-// userSchema.methods.apiGenType = function() {
-
-//   return {
-  
-//     gender:this.gender
-//   };
-// }
-
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
 const UserDetail = mongoose.model('UserDetail', userSchema,'userDetails');
 
 module.exports = {UserDetail};
-// const  acttypeSchema = mongoose.Schema({
-  
-//    acttype:{type: String}
-// });
-// acttypeSchema.methods.apiRepr = function() {
-
-//   return {
-   
-//     acttype:this.acttype
-//   };
-// }
-
-// const ActTypeDetail = mongoose.model('ActTypeDetail', acttypeSchema,'ActTypeDetail');
-
-// module.exports = {ActTypeDetail};
